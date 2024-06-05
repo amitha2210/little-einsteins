ad3001db7cd5365f6c4e62199ba8dc04b36cb4eb
undeadarmour5 <gladwintan@hotmail.sg>
2024-06-05 19:56:57 +0800

    added google login

---
+++ src/app/(auth)/login/page.jsx
@@ -0,0 +1,11 @@
+import GoogleLogin from "@/components/googleLogin"
+
+function login() {
+    return (
+        <div>
+            <GoogleLogin />
+        </div>
+    )
+}
+
+export default login
---
+++ src/app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,2 @@
+import { handlers } from "@/utils/auth" // Referring to the auth.js we just created
+export const { GET, POST } = handlers
