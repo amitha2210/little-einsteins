19340f1e1216eee21367836ac0c3547d53971063
undeadarmour5 <gladwintan@hotmail.sg>
2024-06-13 17:50:24 +0800

    hella

---
+++ src/utils/gemini2.js
@@ -0,0 +1,21 @@
+"use server"
+import 'dotenv/config'
+import { GoogleGenerativeAI } from "@google/generative-ai";
+// Access your API key as an environment variable (see "Set up your API key" above)
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+
+export default async function run(previousState, formData) {
+
+    const { prompt } = Object.fromEntries(formData)
+  // The Gemini 1.5 models are versatile and work with both text-only and multimodal prompts
+  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
+
+  
+
+  const result = await model.generateContent(prompt);
+  const response = result.response;
+  const text = response.text();
+ 
+  console.log(text);
+  return text;
+}
