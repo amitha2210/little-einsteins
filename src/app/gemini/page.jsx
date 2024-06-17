b65825364ce6c99dec8df25e78da6f2785bff388
amitha2210 <amithavuputuri@yahoo.com.sg>
2024-06-17 16:30:18 +0800

    pre

--- src/app/gemini/page.jsx
+++ src/app/gemini/page.jsx
@@ -6,12 +6,13 @@ function Gemini() {
     const [state, formAction] = useFormState(generateText, "")
 
     return (
-        <div>
+        <div> <pre className="overflow-x-auto whitespace-pre-wrap">
             <form action={formAction} className="mt-[8rem]">
                 <input type="text" placeholder="Input prompt" name="prompt" required />
                 <button type="submit">Submit</button>
                 {state}           
-            </form>
+            </form> 
+            </pre>
         </div>        
     )
    
