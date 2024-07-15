a4dca08226d54fee1853baacd05dcb825c07c932
amitha2210 <amithavuputuri@yahoo.com.sg>
2024-07-15 12:53:05 +0800

    dest

--- src/app/(root)/preferences/page.jsx
+++ src/app/(root)/preferences/page.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
+import GoogleMap from "@/components/googleMap/GoogleMap"
+import { auth } from "@/utils/auth/auth"
 import Dates from '@/components/preferencesPage/calendar/calender'
 
 
-function Preferences() {
+async function Preferences() {
+
+  const session = await auth()
+  
   return (
-    <div >
-      <Dates />
+    <div>
+      <Destination session={session}/>
+     <Dates />
     </div>
   )
 }
 
 export default Preferences
+
