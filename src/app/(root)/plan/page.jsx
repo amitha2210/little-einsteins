4d5f57123a1cf5c380f2947646c94ec5762daaf6
gladwintan <gladwintan@hotmail.sg>
2024-08-16 15:23:17 +0800

    updated plan page

--- src/app/(root)/plan/page.jsx
+++ src/app/(root)/plan/page.jsx
@@ -1,23 +1,19 @@
-"use client"
 import React from 'react'
-//import { auth } from "@/utils/auth/auth"
+import { auth } from "@/utils/auth/auth"
 import Preferences from '@/components/preferencesPage/preferences'
-import generateText from "@/utils/gemini2"
-import { useFormState } from "react-dom"
 
-function Plan() {
-    async function plan2() {
-        const [state, formAction] = useFormState(generateText, "")
-        const session = await auth()
+async function Plan() {
+        
+    const session = await auth()
 
-        return (
+    return (
+        <div>
             <div>
-                <div>
-                    <Preferences session={session}/>
-                </div>
-            </div>      
-        )
-    }
+                <Preferences session={session}/>
+            </div>
+        </div>      
+    )
+    
 }
 
 export default Plan
